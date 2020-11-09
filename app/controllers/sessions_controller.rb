b5d37f6e0cff1f39a6f6a4416bbdc29c841d4b48
SpaYco <zizou.aziz101@gmail.com>
2020-11-10 00:59:54 +0100

    fixed variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
     def signin
         user = User.where(username: params[:username])
         if !user.nil?
-            render json: reply: 'exists', user, status: 200, location: @user
+            render :json => { reply: 'exists', user, status: 200, location: user }
         else
-            render json: reply: 'noexists', @user.errors, status: :unprocessable_entity
+            render :json => { reply: 'noexists', user.errors, status: :unprocessable_entity }
         end
     end
 
     def error 
-        render json: reply: 'denied';
+        render :json =>  { reply: 'denied' }
     end
 
 end
