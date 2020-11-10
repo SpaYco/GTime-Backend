501a80c281a85443677ef25f2c21b274f92269dd
SpaYco <zizou.aziz101@gmail.com>
2020-11-10 01:22:54 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :categories, only: [ :create, :index, :show]
   resources :gtimes, only: [ :create,  :show]
 
-  post '/signin', to: 'sessions#signin'
+  get '/signin', to: 'sessions#signin'
   get '/error', to: 'sessions#error'
 
   root 'categories#index'
