8f1aa1e58c288b963fddb811466f89645192a849
SpaYco <zizou.aziz101@gmail.com>
2020-11-10 01:10:53 +0100

    fixed sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
     def signin
         user = User.where(username: params[:username])
         if !user.nil?
-            render :json => { reply: 'exists', user, status: 200, location: user }
+            render :json => { reply: 'exists', user: user, status: 200, location: user }
         else
-            render :json => { reply: 'noexists', user.errors, status: :unprocessable_entity }
+            render :json => { reply: 'noexists', user: user.errors, status: :unprocessable_entity }
         end
     end
 
