64b784c28a9813fb1c1c548ad52af86d447daad8
SpaYco <zizou.aziz101@gmail.com>
2020-11-20 14:14:54 +0100

    limit skip before action to only create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, only: :create
 
   def index
     games = Game.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token
   before_action :logged?, except: :create
 
   def show
